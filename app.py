45d89ee9cde2528ff780e25e3f91b530fc815991
Chris Anderson <traveller.anderson@gmail.com>
2019-06-10 16:23:15 +0100

    Change the IP and PORT for Heroku

--- app.py
+++ app.py
@@ -340,4 +340,5 @@ def postrecipe():
 # Main loop - will need to be changed when hosting on Heroku
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(host="0.0.0.0", port="8080", debug=False)
+
+    app.run(host=os.environ.get("IP"), port=os.environ.get("PORT"), debug=False)
